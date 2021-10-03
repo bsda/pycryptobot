02853744f43202c386fa1bd462b82a7a855e0e41
Bruno <bsda@users.noreply.github.com>
2021-10-03 20:19:26 +0100

    meh

--- pycryptobot.py
+++ pycryptobot.py
@@ -1009,9 +1009,9 @@ def executeJob(sc=None, app: PyCryptoBot=None, state: AppState=None, trading_dat
             # poll every 1 minute
             list(map(s.cancel, s.queue))
             if margin > 5:
-                poll = 15
+                poll = 5
             else:
-                poll = 30
+                poll = 5
             s.enter(poll, 1, executeJob, (sc, app, state))
 
 
